0f2298848dd9b08bde270d3a16d1c80282dff4b9
Fviramontes8 <kuolemajarvi@yahoo.com>
2020-08-24 18:00:39 -0600

    Added some basic documentation on the functions in LinkedListSingle/ssl_functions.c

--- LinkedListSingle/sll_functions.c
+++ LinkedListSingle/sll_functions.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "single_linked_list.h"
 
+// Takes a pointer to a linked list node, will print until it reaches NULL
 void print_list(SingleLinkedList *node) {
 	SingleLinkedList *temp;
 	for(temp = node; temp != NULL; temp = temp->next) {
@@ -9,11 +10,14 @@ void print_list(SingleLinkedList *node) {
 	printf("\n");
 }
 
+// Will add new_node after base_node, so base_node will point to new_node
 void add_node(SingleLinkedList *base_node, SingleLinkedList *new_node) {
 	new_node->next = base_node->next;
 	base_node->next = new_node;
 }
 
+// Removes the linked list node after base_node, if it is at the end of the
+//	list, nothing is done.
 void rm_node(SingleLinkedList *base_node) {
 	if (base_node->next != NULL) {
 		base_node->next = base_node->next->next;
